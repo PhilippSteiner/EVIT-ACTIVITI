0978b69f5a6dfa180fd0727d40599e9642651bae
mikevinmike <michael.dabernig@gmail.com>
2013-04-30 13:25:25 +0200

    source und webapp getrennt; webprocess projekt, das JSF und Activiti vereint (now added)

---
+++ sources/FirstProcess/src/test/java/org/activiti/designer/test/start.java
@@ -0,0 +1,19 @@
+package org.activiti.designer.test;
+
+public class start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+			new ProcessTestMyProcess().startProcess();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
