95bb3cbe55c8b3256f37ee489581d0ea23826504
PhilippSteiner <philipp.steiner@snw.at>
2013-05-27 19:19:04 +0200

    a

--- sources/BMWEventInvitation/target/generated-sources/annotations/ac/at/fhkufstein/entity/BmwFlight_.java
+++ sources/BMWEventInvitation/target/generated-sources/annotations/ac/at/fhkufstein/entity/BmwFlight_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-05-26T21:25:40")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-05-26T21:58:11")
 @StaticMetamodel(BmwFlight.class)
 public class BmwFlight_ { 
 
