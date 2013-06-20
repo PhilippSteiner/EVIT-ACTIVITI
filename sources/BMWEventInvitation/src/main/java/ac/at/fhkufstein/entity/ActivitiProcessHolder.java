b9d39fd0ce8206b0d858eacf1a26e832a43d85cd
mikevinmike <michael.dabernig@gmail.com>
2013-06-20 13:53:46 +0200

    Verbindung der Views und der Applikationslogik mit dem Activiti Prozess

---
+++ sources/BMWEventInvitation/src/main/java/ac/at/fhkufstein/entity/ActivitiProcessHolder.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.at.fhkufstein.entity;
+
+/**
+ *
+ * @author mike
+ */
+public interface ActivitiProcessHolder {
+
+    public Integer getId();
+
+    public void setId(Integer id);
+
+    public Integer getProcessId();
+
+    public void setProcessId(Integer processId);
+
+}
