14652242b12eeeeecea12e673b826c066c402371
mikevinmike <michael.dabernig@gmail.com>
2013-06-25 15:43:03 +0200

    Dokumentation Prozesse

---
+++ sources/BMWEventInvitation/src/main/java/ac/at/fhkufstein/activiti/delegates/CancelInvitation.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.at.fhkufstein.activiti.delegates;
+
+import ac.at.fhkufstein.activiti.InvitationProcess;
+import ac.at.fhkufstein.bean.BmwEventController;
+import ac.at.fhkufstein.bean.BmwParticipantsController;
+import ac.at.fhkufstein.entity.BmwEvent;
+import ac.at.fhkufstein.entity.BmwParticipants;
+import ac.at.fhkufstein.service.PersistenceService;
+import java.util.Date;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+/**
+ *
+ * @author mike
+ */
+public class CancelInvitation implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution execution) {
+
+
+        System.out.println("################# sending cancel invitation mails #################");
+
+
+        BmwParticipants participant = (BmwParticipants) PersistenceService.loadByInteger(BmwParticipantsController.class, execution.getVariable(InvitationProcess.DATABASE_PARTICIPANTID));
+        BmwEvent event = (BmwEvent) PersistenceService.loadByInteger(BmwEventController.class, execution.getVariable(InvitationProcess.DATABASE_EVENTID));
+
+        try {
+
+                // @todo implementMailFunction
+//            MailService.sendMail(null, null, null);
+
+                String mailSentMessage = "Es wurde eine Stornierungsmail an den Teilnehmer " + participant.getUserId().getPersonenID().getVorname() + " " + participant.getUserId().getPersonenID().getNachname() + " gesendet.";
+
+                System.out.println(mailSentMessage);
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(mailSentMessage));
+
+
+        } catch (Exception ex) {
+            String mailSentMessage = ex.getMessage();
+
+            System.err.println(mailSentMessage);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, null, mailSentMessage));
+        }
+
+    }
+}
