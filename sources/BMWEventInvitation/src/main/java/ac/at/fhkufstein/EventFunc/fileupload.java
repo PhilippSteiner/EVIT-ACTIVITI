0a44e62554559bc64acaf6fdc9c8e08c41580e4c
axe100 <alex_ries@gmx.de>
2013-06-27 05:09:48 +0200

    commit 5 9

--- sources/BMWEventInvitation/src/main/java/ac/at/fhkufstein/EventFunc/fileupload.java
+++ sources/BMWEventInvitation/src/main/java/ac/at/fhkufstein/EventFunc/fileupload.java
@@ -6,8 +6,7 @@ package ac.at.fhkufstein.EventFunc;
 
 /**
  *
- * @author
- * wolfgangteves
+ * @author wolfgangteves
  */
 import java.io.File;
 import java.io.FileInputStream;
@@ -33,94 +32,100 @@ import org.primefaces.model.StreamedContent;
 @ManagedBean(name = "fileupload")
 @ViewScoped
 public class fileupload {
-	//This is how you set a property in java
-	//System.setProperty("event_upl_path", "/Volumes/Macintosh HD/Uploads");
+    //This is how you set a property in java
+    //System.setProperty("event_upl_path", "/Volumes/Macintosh HD/Uploads");
 
-	FacesContext facesContext = FacesContext.getCurrentInstance();
-	public Integer eventID = (Integer) Integer.parseInt(facesContext.getExternalContext().getRequestParameterMap().get("eventID"));
-	String dir = System.getProperty("event_upl_path") + "/" + eventID.toString();//Path where the files are saved
+    /*String getPropertyStringRaw = System.getProperty("event_upl_path");
+    String getPropertyString = getPropertyStringRaw.replace("\\", "\\\\");*/
+    
+    
+    FacesContext facesContext = FacesContext.getCurrentInstance();
+    public Integer eventID = (Integer) Integer.parseInt(facesContext.getExternalContext().getRequestParameterMap().get("eventID"));
+    String dir = System.getProperty("event_upl_path") + "/" + eventID.toString();//Path where the files are saved
+    //String dir = getPropertyString + "/" + eventID.toString();//Path where the files are saved
 
-	public void handleFileUpload(FileUploadEvent event) throws IOException {
+    public void handleFileUpload(FileUploadEvent event) throws IOException {
 
-		System.out.println("Saving:" + event.getFile().getFileName());
+        System.out.println("Saving:" + event.getFile().getFileName());
 
-
-		Boolean success = (new File(dir)).mkdirs();//Create if needed
-		if (success) {
-			System.out.println("Folder Created");
-		}
-
-
-		if (new File(dir + "/", event.getFile().getFileName()).createNewFile()) {//create File
-			System.out.println("File Created");
-		} else {
-			System.out.println("Cant Create");
-		}
-
-		InputStream input = event.getFile().getInputstream();//Get File contents
-		OutputStream output = new FileOutputStream(new File(dir, event.getFile().getFileName()));//prepare write
-
-		try {
-			IOUtils.copy(input, output);//write
-		} finally {
-			IOUtils.closeQuietly(input);
-			IOUtils.closeQuietly(output);
-		}
-		//Send Message
-		FacesContext facesContext2 = FacesContext.getCurrentInstance();
-		FacesMessage msg = new FacesMessage("Erfolg", event.getFile().getFileName() + " wurde erfolgreich hochgeladen.");
-		facesContext2.addMessage(null, msg);
-
-	}
-
-	public List<File> getEventFolder() {
+        Boolean success = (new File(dir)).mkdirs();//Create if needed
+        if (success) {
+            System.out.println("Folder Created");
+        }
 
 
-		final File folder = new File(dir);
-		return listFilesForFolder(folder);
-	}
+        if (new File(dir + "/", event.getFile().getFileName()).createNewFile()) {//create File
+            System.out.println("File Created");
+        } else {
+            System.out.println("Cant Create");
+        }
 
-	public List<File> listFilesForFolder(final File folder) {
-		List<File> files = new ArrayList<File>();
-		if (folder.length() > 0) {
-			for (final File fileEntry : folder.listFiles()) {
-				if (fileEntry.isDirectory()) {
-					listFilesForFolder(fileEntry);
-				} else {
-					files.add(fileEntry);
-					System.out.println(fileEntry.getName());
-				}
-			}
-		} else {
-			System.out.println("Keine Dateien Vorhanden");
-		}
-		return files;
-	}
+        InputStream input = event.getFile().getInputstream();//Get File contents
+        OutputStream output = new FileOutputStream(new File(dir, event.getFile().getFileName()));//prepare write
 
-	public StreamedContent getCurrentEventFile(String name) {
-		
-		try{
+        try {
+            IOUtils.copy(input, output);//write
+        } finally {
+            IOUtils.closeQuietly(input);
+            IOUtils.closeQuietly(output);
+        }
+        //Send Message
+        FacesContext facesContext2 = FacesContext.getCurrentInstance();
+        FacesMessage msg = new FacesMessage("Erfolg", event.getFile().getFileName() + " wurde erfolgreich hochgeladen.");
+        facesContext2.addMessage(null, msg);
 
-		System.out.println("Opening File..."+name);
-		
-		File f = new File(dir + "/" + name);
-		System.out.println("Got File"+f.getName());
-		InputStream stream = new FileInputStream(dir + "/" + name);
+    }
 
-		return new DefaultStreamedContent(stream, "application", f.getName());
-		}catch(Exception e){
-			System.out.println("File nicht gefunden");
-			return null;
-		}
-	}
+    public List<File> getEventFolder() {
 
-	public Integer getEventID() {
+        //System.setProperty("event_upl_path", "C:\\BMW_Folder");
+        //System.out.println("Property set to: "+System.getProperty("event_upl_path"));
 
-		return eventID;
-	}
+        final File folder = new File(dir);
+        return listFilesForFolder(folder);
+    }
 
-	public void setEventID(Integer eventID) {
-		this.eventID = eventID;
-		System.out.println("Set:" + eventID);
-	}
+    public List<File> listFilesForFolder(final File folder) {
+        List<File> files = new ArrayList<File>();
+        if (folder.listFiles().length > 0) {
+            for (final File fileEntry : folder.listFiles()) {
+                if (fileEntry.isDirectory()) {
+                    listFilesForFolder(fileEntry);
+                } else {
+                    files.add(fileEntry);
+                    System.out.println(fileEntry.getName());
+                }
+            }
+        } else {
+            System.out.println("Keine Dateien Vorhanden");
+        }
+        return files;
+    }
+
+    public StreamedContent getCurrentEventFile(String name) {
+
+        try {
+
+            System.out.println("Opening File..." + name);
+
+            File f = new File(dir + "/" + name);
+            System.out.println("Got File" + f.getName());
+            InputStream stream = new FileInputStream(dir + "/" + name);
+
+            return new DefaultStreamedContent(stream, "application", f.getName());
+        } catch (Exception e) {
+            System.out.println("File nicht gefunden");
+            return null;
+        }
+    }
+
+    public Integer getEventID() {
+
+        return eventID;
+    }
+
+    public void setEventID(Integer eventID) {
+        this.eventID = eventID;
+        System.out.println("Set:" + eventID);
+    }
 }
